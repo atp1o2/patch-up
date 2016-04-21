9dc93298abaabfea6df529892c483e61ab267269
atp1o2 <andrewpham8@gmail.com>
2016-04-21 15:06:31 -0700

    basic feed format

--- js/controllers/feedCtrl.js
+++ js/controllers/feedCtrl.js
@@ -3,5 +3,23 @@
 angular.module('patchUp')
 
 .controller('feedCtrl', function ($scope) {
-  $scope.message = '';
+  
+
+  $scope.posts = [
+    { name: 'POST', date: '04/21', content: 'blahahahahahahahahahhahahahahah'},
+    { name: 'POST1', date: '04/21', content: 'blahahahahahahahahahhahahahahah'},
+    { name: 'POST2', date: '04/21', content: 'blahahahahahahahahahhahahahahah'},
+    { name: 'POST3', date: '04/21', content: 'blahahahahahahahahahhahahahahah'},
+    { name: 'POST4', date: '04/21', content: 'blahahahahahahahahahhahahahahah'},
+    { name: 'POST5', date: '04/21', content: 'blahahahahahahahahahhahahahahah'},
+    { name: 'POST6', date: '04/21', content: 'blahahahahahahahahahhahahahahah'},
+    { name: 'POST7', date: '04/21', content: 'blahahahahahahahahahhahahahahah'},
+    { name: 'POST8', date: '04/21', content: 'blahahahahahahahahahhahahahahah'},
+    { name: 'POST9', date: '04/21', content: 'blahahahahahahahahahhahahahahah'},
+    { name: 'POST10', date: '04/21', content: 'blahahahahahahahahahhahahahahah'},
+  ]
+
+
+
+
 });
